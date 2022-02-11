ed79f0ab8943de2b56a970c458793e2cc0f5281d
Kamil Jamróz <nergydev@gmail.com>
2022-02-11 08:50:23 +0100

    implemented texture resources

--- Nickel/Source/Renderer/Direct3D12/Resources.cpp
+++ Nickel/Source/Renderer/Direct3D12/Resources.cpp
@@ -1,5 +1,6 @@
 #include "Resources.h"
 #include "D3D12Core.h"
+#include "D3D12Helpers.h"
 
 namespace Nickel::Renderer::DX12Layer {
 	auto DescriptorHeap::Initialize(u32 _capacity, bool isShaderVisible) -> bool {
@@ -104,5 +105,107 @@ namespace Nickel::Renderer::DX12Layer {
 		deferredFreeIndices[frameIndex].push_back(index);
 		Core::SetDeferredReleasesFlag();
 		handle = {};
+	}
+
+	// --- D3D12Texture ---
+	D3D12Texture::D3D12Texture(D3D12TextureInitData data) {
+		auto* const device{ Core::GetDevice() };
+		Assert(device != nullptr);
+
+		D3D12_CLEAR_VALUE* const clearVal = (data.desc && (data.desc->Flags & D3D12_RESOURCE_FLAG_ALLOW_RENDER_TARGET ||
+			data.desc->Flags & D3D12_RESOURCE_FLAG_ALLOW_DEPTH_STENCIL))
+			? &data.clearValue : nullptr;
+		if (data.resource) {
+			Assert(data.heap == nullptr);
+			resource = data.resource;
+		}
+		else if (data.heap != nullptr && data.desc != nullptr) {
+			Assert(data.resource == nullptr);
+			ASSERT_ERROR_RESULT(device->CreatePlacedResource(data.heap, data.allocationInfo.Offset, data.desc, data.initialState, clearVal, IID_PPV_ARGS(&resource)));
+		} else if (data.desc != nullptr) {
+			Assert(data.resource != nullptr && data.heap == nullptr);
+			const auto& defaultHeap = primal::graphics::d3d12::d3dx::heapProperties.defaultHeap;
+			ASSERT_ERROR_RESULT(device->CreateCommittedResource(&defaultHeap, D3D12_HEAP_FLAGS::D3D12_HEAP_FLAG_NONE, data.desc, data.initialState, clearVal, IID_PPV_ARGS(&resource)));
+		}
+
+		Assert(resource != nullptr);
+		srv = Core::GetSrvHeap().Allocate();
+		device->CreateShaderResourceView(resource, data.srvDesc, srv.cpu);
+	}
+
+	auto D3D12Texture::Release() -> void {
+		Core::GetSrvHeap().Free(srv);
+		Core::SafeDeferredRelease(resource);
+	}
+
+	// --- D3D12RenderTexture ---
+	D3D12RenderTexture::D3D12RenderTexture(D3D12TextureInitData data)
+		: texture{ data }
+	{
+		Assert(data.desc);
+		mipCount = GetResource()->GetDesc().MipLevels;
+		Assert(mipCount && mipCount <= D3D12Texture::MAX_MIPS);
+
+		DescriptorHeap& rtvHeap{ Core::GetRtvHeap() };
+		D3D12_RENDER_TARGET_VIEW_DESC desc{};
+		desc.Format = data.desc->Format;
+		desc.ViewDimension = D3D12_RTV_DIMENSION_TEXTURE2D;
+		desc.Texture2D.MipSlice = 0;
+
+		auto* const device{ Core::GetDevice() };
+		assert(device);
+
+		for (u32 i = 0; i < mipCount; ++i) {
+			rtv[i] = rtvHeap.Allocate();
+			device->CreateRenderTargetView(GetResource(), &desc, rtv[i].cpu);
+			++desc.Texture2D.MipSlice;
+		}
+	}
+
+	void D3D12RenderTexture::Release() {
+		for (u32 i{ 0 }; i < mipCount; ++i)
+			Core::GetRtvHeap().Free(rtv[i]);
+		texture.Release();
+		mipCount = 0;
+	}
+
+	// --- D3D12DepthBuffer ---
+	D3D12DepthBuffer::D3D12DepthBuffer(D3D12TextureInitData data) {
+		Assert(data.desc);
+		const DXGI_FORMAT dsvFormat{ data.desc->Format };
+
+		D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc{};
+		if (data.desc->Format == DXGI_FORMAT_D32_FLOAT) {
+			data.desc->Format = DXGI_FORMAT_R32_TYPELESS;
+			srvDesc.Format = DXGI_FORMAT_R32_FLOAT;
+		}
+
+		srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
+		srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
+		srvDesc.Texture2D.MipLevels = 1;
+		srvDesc.Texture2D.MostDetailedMip = 0;
+		srvDesc.Texture2D.PlaneSlice = 0;
+		srvDesc.Texture2D.ResourceMinLODClamp = 0.f;
+
+		Assert(!data.srvDesc && !data.resource);
+		data.srvDesc = &srvDesc;
+		texture = D3D12Texture(data);
+
+		D3D12_DEPTH_STENCIL_VIEW_DESC dsvDesc{};
+		dsvDesc.ViewDimension = D3D12_DSV_DIMENSION_TEXTURE2D;
+		dsvDesc.Flags = D3D12_DSV_FLAG_NONE;
+		dsvDesc.Format = dsvFormat;
+		dsvDesc.Texture2D.MipSlice = 0;
+
+		dsv = Core::GetDsvHeap().Allocate();
+
+		auto* const device{ Core::GetDevice() };
+		assert(device);
+		device->CreateDepthStencilView(GetResource(), &dsvDesc, dsv.cpu);
+	}
+
+	void D3D12DepthBuffer::Release() {
+		Core::GetDsvHeap().Free(dsv);
+		texture.Release();
 	}
 }
--- Nickel/Source/Renderer/Direct3D12/Resources.h
+++ Nickel/Source/Renderer/Direct3D12/Resources.h
@@ -7,6 +7,7 @@ namespace Nickel::Renderer::DX12Layer {
 	struct DescriptorHandle {
 		D3D12_CPU_DESCRIPTOR_HANDLE cpu{};
 		D3D12_GPU_DESCRIPTOR_HANDLE gpu{};
+		u32 index = u32_invalid_id;
 
 		constexpr auto IsValid() const -> bool { return cpu.ptr != 0; }
 		constexpr auto IsShaderVisible() const -> bool { return gpu.ptr != 0; }
@@ -15,7 +16,6 @@ namespace Nickel::Renderer::DX12Layer {
 #ifdef _DEBUG
 		friend class DescriptorHeap;
 		DescriptorHeap* container = nullptr;
-		u32 index = 0;
 #endif
 	};
 
@@ -26,20 +26,20 @@ namespace Nickel::Renderer::DX12Layer {
 		DescriptorHeap() = default;
 		explicit DescriptorHeap(DescriptorHeap& o) = delete;
 		explicit DescriptorHeap(DescriptorHeap&& o) = delete;
-		auto operator=(const DescriptorHeap&) -> DescriptorHeap & = delete;
-		auto operator=(const DescriptorHeap&&) -> DescriptorHeap & = delete;
+		auto operator=(const DescriptorHeap&)->DescriptorHeap & = delete;
+		auto operator=(const DescriptorHeap&&)->DescriptorHeap & = delete;
 
 		auto Initialize(u32 capacity, bool isShaderVisible) -> bool;
 		auto ProcessDeferredFree(u32 frameIndex) -> void;
 		auto Release() -> void;
 
-		[[nodiscard]] auto Allocate() -> DescriptorHandle;
+		[[nodiscard]] auto Allocate()->DescriptorHandle;
 		auto Free(DescriptorHandle& handle) -> void;
 
 		constexpr auto GetType()      const -> D3D12_DESCRIPTOR_HEAP_TYPE { return type; };
 		constexpr auto GetCpuStartAddressHandle() const -> D3D12_CPU_DESCRIPTOR_HANDLE { return cpuStartAddressHandle; };
 		constexpr auto GetGpuStartAddressHandle() const -> D3D12_GPU_DESCRIPTOR_HANDLE { return gpuStartAddressHandle; };
-		constexpr auto GetHeap()      const -> ID3D12DescriptorHeap *const { return heap; };
+		constexpr auto GetHeap()      const -> ID3D12DescriptorHeap* const { return heap; };
 
 		constexpr auto GetCapacity()       const -> u32 { return capacity; };
 		constexpr auto GetSize()           const -> u32 { return size; };
@@ -60,5 +60,141 @@ namespace Nickel::Renderer::DX12Layer {
 		u32 size = 0;
 		u32 descriptorSize = 0;
 		const D3D12_DESCRIPTOR_HEAP_TYPE type;
+	};
+
+	struct D3D12TextureInitData {
+		ID3D12Heap1* heap;
+		ID3D12Resource* resource = nullptr;
+		D3D12_RESOURCE_ALLOCATION_INFO1 allocationInfo;
+		D3D12_SHADER_RESOURCE_VIEW_DESC* srvDesc = nullptr;
+		D3D12_RESOURCE_DESC* desc = nullptr;
+		D3D12_RESOURCE_STATES initialState{};
+		D3D12_CLEAR_VALUE clearValue;
+	};
+
+	class D3D12Texture {
+	public:
+		constexpr static u32 MAX_MIPS = 14;
+		D3D12Texture() = default;
+		explicit D3D12Texture(D3D12TextureInitData data);
+		explicit D3D12Texture(const D3D12Texture&) = delete;
+		auto operator=(const D3D12Texture&)->D3D12Texture & = delete;
+		constexpr D3D12Texture(D3D12Texture&& o) : resource(o.resource), srv(srv) {
+			o.Reset();
+		}
+
+		constexpr auto operator=(D3D12Texture&& o) -> D3D12Texture& {
+			Assert(this != &o);
+			if (this != &o) {
+				Release();
+				Move(o);
+			}
+			return *this;
+		}
+
+		~D3D12Texture() { Release(); }
+
+		auto Release() -> void;
+		constexpr auto GetResource() const -> ID3D12Resource* const { return resource; }
+		constexpr auto GetSrv() const -> DescriptorHandle const { return srv; }
+	private:
+		constexpr auto Move(D3D12Texture& o) -> void {
+			resource = o.resource;
+			srv = o.srv;
+			o.Reset();
+		}
+
+		constexpr auto Reset() -> void {
+			resource = nullptr;
+			srv = {};
+		}
+
+		ID3D12Resource* resource{ nullptr };
+		DescriptorHandle srv;
+	};
+
+	class D3D12RenderTexture {
+	public:
+		D3D12RenderTexture() = default;
+		explicit D3D12RenderTexture(D3D12TextureInitData data);
+		explicit D3D12RenderTexture(const D3D12RenderTexture&) = delete;
+		auto operator=(const D3D12RenderTexture&) -> D3D12RenderTexture& = delete;
+		constexpr D3D12RenderTexture(D3D12RenderTexture&& o)
+			: texture{ std::move(o.texture) }, mipCount{ o.mipCount }
+		{
+			for (u32 i{ 0 }; i < mipCount; ++i)
+				rtv[i] = o.rtv[i];
+			o.Reset();
+		}
+
+		constexpr auto operator=(D3D12RenderTexture&& o) -> D3D12RenderTexture& {
+			Assert(this != &o);
+			if (this != &o) {
+				Release();
+				Move(o);
+			}
+			return *this;
+		}
+
+		~D3D12RenderTexture() { Release(); }
+
+		void Release();
+		constexpr auto GetMipCount() const -> u32 { return mipCount; }
+		constexpr auto GetRtv(u32 mipIndex) const -> D3D12_CPU_DESCRIPTOR_HANDLE { Assert(mipIndex < mipCount); return rtv[mipIndex].cpu; }
+		constexpr auto GetSrv() const -> DescriptorHandle { return texture.GetSrv(); }
+		constexpr auto GetResource() const -> ID3D12Resource* const { return texture.GetResource(); }
+	private:
+		constexpr auto Move(D3D12RenderTexture& o) -> void {
+			texture = std::move(o.texture);
+			mipCount = o.mipCount;
+			for (u32 i{ 0 }; i < mipCount; ++i)
+				rtv[i] = o.rtv[i];
+			o.Reset();
+		}
+
+		constexpr auto Reset() -> void {
+			for (u32 i{ 0 }; i < mipCount; ++i)
+				rtv[i] = {};
+			mipCount = 0;
+		}
+
+		D3D12Texture       texture{};
+		DescriptorHandle   rtv[D3D12Texture::MAX_MIPS]{};
+		u32                mipCount{ 0 };
+	};
+
+	class D3D12DepthBuffer {
+	public:
+		D3D12DepthBuffer() = default;
+		explicit D3D12DepthBuffer(D3D12TextureInitData data);
+		explicit D3D12DepthBuffer(const D3D12RenderTexture&) = delete;
+		auto operator=(const D3D12DepthBuffer&) -> D3D12DepthBuffer& = delete;
+
+		constexpr D3D12DepthBuffer(D3D12DepthBuffer&& o)
+			: texture{ std::move(o.texture) }, dsv{ o.dsv }
+		{
+			o.dsv = {};
+		}
+
+		constexpr auto operator=(D3D12DepthBuffer&& o) -> D3D12DepthBuffer& {
+			Assert(this != &o);
+			if (this != &o) {
+				texture = std::move(o.texture);
+				dsv = o.dsv;
+				o.dsv = {};
+			}
+			return *this;
+		}
+
+		~D3D12DepthBuffer() { Release(); }
+
+		void Release();
+		constexpr auto GetDsv() const -> D3D12_CPU_DESCRIPTOR_HANDLE { return dsv.cpu; }
+		constexpr auto GetSrv() const -> DescriptorHandle { return texture.GetSrv(); }
+		constexpr auto GetResource() const ->ID3D12Resource* const { return texture.GetResource(); }
+
+	private:
+		D3D12Texture     texture{};
+		DescriptorHandle dsv{};
 	};
 }
